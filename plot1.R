b7af4b34dfb5e9bcbf7d623b1a321ff4acdcc54a
marioem <marioem@users.noreply.github.com>
2015-09-20 19:11:36 +0200

    Added comments, plots beautified and generated to files, tasks 1-3

--- plot1.R
+++ plot1.R
@@ -1,12 +1,45 @@
-require(dplyr)
+#################################################################################
+#
+#  Filename: plot1.R
+#   Version: 1.0.0
+#      Date: 2015.09.20
+#    Author: Mariusz Musiał
+# Rev. Info: 1.0 - initial version of the script
+# 
+# This script generates a plot of total PM25 emissions in US in years 1999-2008.
+# Base grapics system is used.
+# Original data is available at: https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2FNEI_data.zip
+# More information about data: http://www.epa.gov/ttn/chief/eiinformation.html
+# 
+#     Input: summarySCC_PM25.rds file located in the current working directory
+#    Output: plot1.png written to the current working directory
+# Execution: source(plot1.R)
+# 
+# Prerequisites: 1) this script and the data files are located in the current
+#                   working directory
+#                2) dplyr package is installed
+# 
+#################################################################################
+
+if(!require("dplyr")) {
+    stop("Please install dplyr package.")
+}
+
+summaryFile <- "summarySCC_PM25.rds"
+pngFile <- "plot1.png"
 
 pm <- readRDS("summarySCC_PM25.rds")
-scc <- readRDS("Source_Classification_Code.rds")
 
+# Calculate total emissions levels for each year
 pmtot <- pm %>% group_by(year) %>% summarise(Emissions = sum(Emissions))
 
+# Plot the emissions levels vs. year along with the linear regression line
+png(pngFile)
 plot(pmtot, ylab = "Emissions [tons]", xaxt = "n")
 abline(lm(Emissions ~ year, pmtot),col = "red")
-legend("topright", legend = c("Total Emissions", "Lin. Regr. Tot. Emiss."), col = c("black", "red"), pch = c(1,45))
 title(main = expression('U.S. Total PM'[2.5]*' Emissions in Years 1999-2008'))
 axis(side = 1, at = pmtot$year,labels = T)
+abline(h = seq(par()$yaxp[1], par()$yaxp[2], length.out = par()$yaxp[3] + 1), col="lightgray", lty="dotted")
+abline(v = pmtot$year, col="lightgray", lty="dotted")
+legend("topright", legend = c("Total Emissions", "Lin. Regr. Tot. Emiss."), col = c("black", "red"), pch = c(1,45))
+dev.off()
--- plot3.R
+++ plot3.R
@@ -1,24 +1,25 @@
 #################################################################################
 #
-#  Filename: plot2.R
+#  Filename: plot3.R
 #   Version: 1.0.0
 #      Date: 2015.09.20
 #    Author: Mariusz Musiał
 # Rev. Info: 1.0 - initial version of the script
 # 
-# This script generates a plot of total PM25 emissions in Baltimore City in years
-# 1999-2008.
-# Base grapics system is used.
+# This script generates a plot of PM25 emissions in Baltimore City in years
+# 1999-2008 split between source types.
+# ggplot2 grapics system is used.
 # Original data is available at: https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2FNEI_data.zip
 # More information about data: http://www.epa.gov/ttn/chief/eiinformation.html
 # 
 #     Input: summarySCC_PM25.rds file located in the current working directory
-#    Output: plot2.png written to the current working directory
-# Execution: source(plot2.R)
+#    Output: plot3.png written to the current working directory
+# Execution: source(plot3.R)
 # 
 # Prerequisites: 1) this script and the data files are located in the current
 #                   working directory
 #                2) dplyr package is installed
+#                3) ggplot2 package is installed
 # 
 #################################################################################
 
@@ -26,23 +27,27 @@ if(!require("dplyr")) {
     stop("Please install dplyr package.")
 }
 
+if(!require("ggplot2")) {
+    stop("Please install ggplot3 package.")
+}
+
 summaryFile <- "summarySCC_PM25.rds"
-pngFile <- "plot2.png"
+pngFile <- "plot3.png"
 
 BaltimoreFips <- "24510"
 
 pm <- readRDS(summaryFile)
 
 # Calculate total emissions levels in Baltimore City for each year
-baltpmtot <- pm %>% filter(fips == BaltimoreFips) %>% group_by(year) %>% summarise(Emissions = sum(Emissions))
+baltpmtype <- pm %>% filter(fips == BaltimoreFips) %>% group_by(type, year) %>% summarise(Emissions = sum(Emissions))
 
 # Plot the emissions levels vs. year along with the linear regression line
-png(pngFile)
-plot(baltpmtot, ylab = "Emissions [tons]", xaxt = "n")
-abline(lm(Emissions ~ year, baltpmtot),col = "red")
-title(main = expression('Baltimore City Total PM'[2.5]*' Emissions in Years 1999-2008'))
-axis(side = 1, at = pmtot$year,labels = T)
-abline(h = seq(par()$yaxp[1], par()$yaxp[2], length.out = par()$yaxp[3] + 1), col="lightgray", lty="dotted")
-abline(v = pmtot$year, col="lightgray", lty="dotted")
-legend("bottomleft", legend = c("Total Emissions", "Lin. Regr. Tot. Emiss."), col = c("black", "red"), pch = c(1,45))
+png(pngFile, width = 640, units = "px")
+ggpl <- ggplot(data = baltpmtype, aes(year, Emissions, color = type))
+ggpl <- ggpl + geom_point(size = 4)
+ggpl <- ggpl + geom_smooth(method = "lm", se = F)
+ggpl <- ggpl + ylab("Emissions [tons]")
+ggpl <- ggpl + ggtitle(expression('Baltimore City PM'[2.5]*' Emission per Source Type in Years 1999-2008'))
+ggpl <- ggpl + scale_x_continuous(breaks = baltpmtype$year, labels = baltpmtype$year)
+ggpl
 dev.off()
